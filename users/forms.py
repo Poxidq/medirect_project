93d65cc3bd44befa9c93dc240e5e22b1b323f96f
Poxidq <yarullin_vadim@mail.ru>
2020-01-25 15:23:39 +0500

    registration form, reset password, change password and other

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,34 @@
+# from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+# from .models import CustomUser
+
+# class CustomUserCreationForm(UserCreationForm):
+
+#     class Meta(UserCreationForm.Meta):
+#         model = CustomUser
+#         fields = UserCreationForm.Meta.fields
+
+# class CustomUserChangeForm(UserChangeForm):
+
+#     class Meta:
+#         model = CustomUser
+#         fields = UserChangeForm.Meta.fields
 from django import forms
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import CustomUser
+from django.contrib.auth.models import User
 
-class CustomUserCreationForm(UserCreationForm):
+class LoginForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
 
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = UserCreationForm.Meta.fields
+class UserRegistrationForm(forms.ModelForm):
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Password', widget=forms.PasswordInput)
 
-class CustomUserChangeForm(UserChangeForm):
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'email')
 
-    class Meta:
-        model = CustomUser
-        fields = UserChangeForm.Meta.fields
+	def clean_password2(self): # проверка на идентичность двух паролей
+		cd = self.cleaned_data
+		if cd['password'] != cd['password2']:
+			raise forms.ValidationError( _ ('Invalid value: %s ') % value)
+		return cd['password2']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, LoginForm
 
 class HomePageView(TemplateView):
     template_name = 'forgot_password.html'
+
+def register(request):
+	if request.method == 'POST'
+		user_form = UserRegistrationForm(request.POST)
+		if user_form.is_valid():
+			new_user = user_form.save(commit = False)
+			new_user.set_password(user_form.cleaned_data['password'])
+			new_user.save()
+			return render(request, 'users/register_done.html', {'user_form': user_form})
+	else:
+		user_form = UserRegistrationForm()
+	return render(request, 'users/register.html', {'user_form': user_form})
+	
 # Create your views here.
